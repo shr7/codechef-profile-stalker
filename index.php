52f31c0b58e1e4111f9529a659500e8195e8c75d
shreya <shreya.chandra96@gmail.com>
2016-06-24 01:11:20 +0530

    script to add user to database

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
 </head>
 
 <body style="background-color:rgba(12, 23, 75, 0.95); color:white; font-family: 'Roboto Slab', serif">
+	
 	<!--Navigation bar-->
     <nav class="navbar navbar-inverse ">
         <div class="container">
@@ -38,7 +39,7 @@
             <center>
                 <div class="col-sm-12">
                     <h2>Stalk User :D</h2>
-                    <form class="" role="form" action="index.php" method="GET">
+                    <form class="" role="form" action="index.php" method="GET" enctype="multipart/form-data">
                         <div class=" row form-group">
                             <label class="col-sm-3" ><h3>Name of the user</h3></label>
                             <div class="col-sm-5">
@@ -47,11 +48,11 @@
                             </div>      
                             <div class="col-sm-2">
                                 <br>
-                                <input type="button" name="stats" value="Track Statistics" class="btn btn-sample">
+                                <input type="submit" name="stats" value="Track Statistics" class="btn btn-sample">
                             </div>      
                             <div class="col-sm-2">
                                 <br>
-                                <input type="button" name="viewProf" value="View Profile"  class="btn btn-sample">
+                                <input type="submit" name="viewProf" value="View Profile"  class="btn btn-sample">
                             </div>  
                         </div>
                     </form>
@@ -60,59 +61,55 @@
         </div>
         <br><br><br><br><br>
         <div class="row">
-            <form>
-                <div class="col-sm-3 col-sm-offset-3">
-                    <input type="submit" name="addUser" value="Add New User" class="btn btn-sample btn-lg" />
-                </div>
-                <div class="col-sm-3">
-                    <input type="submit" name="showdb" value="View Records" class="btn btn-sample btn-lg" />
-                </div>
-            </form>
-
+            <div class="col-sm-3 col-sm-offset-3">
+                <button type="submit" name="addUser" class="btn btn-sample btn-lg" data-toggle="modal" data-target="#addUser">Add New User</button> 
+            </div>
+            <div class="col-sm-3">
+                <button type="submit"  name="showdb" class="btn btn-sample btn-lg" data-toggle="modal" data-target="#showdb">View Records</button>
+            </div>
         </div>
     </div>
     <br>
+    <br>
+    <br>
+
         
     <div class="footer">
       <div class="container-fluid">
            <center><p class="footerText">Codechef Stalker By <a href="">Shreya</a></p></center>
       </div>
     </div>
-<!--
-         <div class="row">
-            <div class="col-sm-7">
-                    <h2>Add User To Database</h2>
-                    <form class="" role="form" action="" method="get">
-                        <div class="row form-group">
-                            <label class="col-sm-4" ><h3>Name of the user</h3></label>
-                            <div class="col-sm-8">
-                                <br>
-                                <input type="text" name="name" class="form-control">
-                            </div>      
-                        </div>
-                        <div class="row form-group">
-                            <label class="col-sm-4" ><h3>Codechef Username</h3></label>
-                            <div class="col-sm-8">
-                                <br>
-                                <input type="text" name="username" class="form-control">
-                            </div>      
-                        </div> 
-                        <div class="form-group">
-                            <div class="col-sm-12">
-                                <br>
-                                <center><input type="button" name="add" value="add" ></center>
-                            </div>      
-                        </div>   
-                    </form>  
-            </div>
-            <div class="col-sm-5">
-                    <br><br><br>
-                    <br><br><br>
-                    <center><button>Show Database</button></center>
-            </div>
-        </div>
-    </div>
--->
+
+    <!--Add User Modal-->
+    <form class="modal fade" id="addUser" action="index.php" method="GET" enctype="multipart/form-data">
+		<div class="modal-dialog modal-md">
+  			<div class="modal-content">
+			    <div class="modal-header">
+        			<button type="button" class="close" data-dismiss="modal">&times;</button>
+       				<h4 class="modal-title">Add New User</h4>
+   				</div>
+       			<div class="modal-body" id="group">
+					<br>
+              	  	<div class="row form-group">
+                    	<label class="col-sm-4" ><center><h4>Name of the user</h4></center></label>
+                    	<div class="col-sm-7">
+                        	<input type="text" name="newname" class="form-control" autocomplete="off" />
+                    	</div>      
+               		 </div>
+                	<div class="row form-group">
+                    	<label class="col-sm-4" ><center><h4>Codechef Username</h4></center></label>
+                    	<div class="col-sm-7">
+                       		<input type="text" name="username" class="form-control" autocomplete="off" />
+                   		</div>      
+                    </div> 
+                </div>
+       			<div class="modal-footer">
+      				<input type="submit" name="newUser" value="Add User" class="btn btn-default"/>
+   				</div>	
+    		</div>
+		</div>	
+	</form>
+
 
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
@@ -124,19 +121,47 @@
 
 <?php
     require_once('resources/init.php');
+    error_reporting(0);
 
-    if(isset($_GET['stats']) || isset($GET['viewProf']))
+	if(isset($_GET['newUser']))
+    {	
+    	$newname=trim($_GET['newname']);
+    	$username=trim($_GET['username']);
+    	if(empty($newname) || empty($username))
+        {
+            echo '<script>alert("Please Fill All Fields")</script>';
+        }
+        else
+        {
+            if(exitsInDatabase($newname))
+            {
+              echo '<script>alert("This name already exists in the database. Please select another name!")</script>';  
+            }
+            else
+            {
+                $url="https://www.codechef.com/users/".$username;
+
+                $insert_query="INSERT INTO subjects (name, cfusername, url) values ('$newname','$username','$url')";
+            }
+            if(mysql_query($insert_query))
+            {
+                echo "<script>alert('Data Inserted In Database')</script>";
+            }
+        }
+
+    }
+    if(isset($_GET['stats']) || isset($_GET['viewProf']))
     {
         $name=trim($_GET['name']);
         if(empty($name))
         {
-            echo 'alert("Please enter the name")';
+            echo '<script>alert("Please enter a name")</script>';
         }
         else
         {
             if(!exitsInDatabase($name))
             {
-              echo 'alert("This name doesn\'t exist in database.<br> Add name and username to database and then you can search as and when wou want!")';  
+              echo '<script>alert("This name doesn\'t exist in database.<br> Add name and username to database and then you can search as and when wou want!")</script>';  
             }
             else
             {
@@ -151,6 +176,9 @@
                 }
             }
         }
-
     }
+
+    
+
+
 ?>
